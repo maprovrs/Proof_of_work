8dfd3c0e70df10632b0c098f255a9d394d16e1e9
maprovrs <gdeamine@gmail.com>
2017-12-14 16:44:27 +0100

    Add files via upload

---
+++ proof_of_work_python_2.7.py
@@ -0,0 +1,20 @@
+
+import hashlib
+
+string = "hello"
+
+complete = False
+n = 0
+
+while complete == False:
+    curr_string = string + str(n)
+    print curr_string
+
+    curr_hash = hashlib.sha256(curr_string).hexdigest()
+    n = n + 1
+
+    print curr_hash
+    if curr_hash.startswith('000'):
+        print curr_hash
+        print curr_string
+complete = True
